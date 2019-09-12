fe046592d4227aceb7e3f05599b5d53b72b08c92
Kyzyurova Evgenia <kyzyurova.evgenia@gmail.com>
2019-09-12 19:23:00 +0300

    Update assignment01a.py

--- assignment01a.py
+++ assignment01a.py
@@ -1,89 +1,23 @@
-"""
-Assignment 1-A
-==============
+a='This is the farmer sowing his corn, '
+b='That kept the cock that crow\'d in the morn, '
+c='That waked the priest all shaven and shorn,'
+d='That married the man all tatter\'d and torn,'
+e='That kissed the maiden all forlorn,'
+f='That milk\'d the cow with the crumpled horn,'
+g='That tossed the dog, '
+h='That worried the cat,'
+i='That killed the rat, '
+j='That ate the malt '
+k='That lay in the house that Jack built.'
 
-Update the Python script below to make it more compact and readable; use at least variables and f-strings.
-For those who are already familiar with Python – write the best code you can to conform to the Zen of Python.
-
-"""
-
-poem = '''
-This is the house that Jack built.
-
-This is the malt 
-That lay in the house that Jack built.
-
-This is the rat, 
-That ate the malt 
-That lay in the house that Jack built.
-
-This is the cat, 
-That kill'd the rat, 
-That ate the malt 
-That lay in the house that Jack built.
-
-This is the dog, 
-That worried the cat, 
-That kill'd the rat, 
-That ate the malt 
-That lay in the house that Jack built.
-
-This is the cow with the crumpled horn, 
-That toss'd the dog, 
-That worried the cat, 
-That kill'd the rat, 
-That ate the malt 
-That lay in the house that Jack built.
-
-This is the maiden all forlorn, 
-That milk'd the cow with the crumpled horn, 
-That tossed the dog, 
-That worried the cat, 
-That kill'd the rat, 
-That ate the malt 
-That lay in the house that Jack built.
-
-This is the man all tatter'd and torn, 
-That kissed the maiden all forlorn, 
-That milk'd the cow with the crumpled horn,
-That tossed the dog, 
-That worried the cat, 
-That kill'd the rat, 
-That ate the malt 
-That lay in the house that Jack built.
-
-This is the priest all shaven and shorn, 
-That married the man all tatter'd and torn, 
-That kissed the maiden all forlorn, 
-That milked the cow with the crumpled horn,
-That tossed the dog, 
-That worried the cat, 
-That kill'd the rat, 
-That ate the malt 
-That lay in the house that Jack built.
-
-This is the cock that crow'd in the morn, 
-That waked the priest all shaven and shorn, 
-That married the man all tatter'd and torn, 
-That kissed the maiden all forlorn, 
-That milk'd the cow with the crumpled horn, 
-That tossed the dog, 
-That worried the cat, 
-That kill'd the rat, 
-That ate the malt 
-That lay in the house that Jack built.
-
-This is the farmer sowing his corn, 
-That kept the cock that crow'd in the morn, 
-That waked the priest all shaven and shorn,
-That married the man all tatter'd and torn, 
-That kissed the maiden all forlorn, 
-That milk'd the cow with the crumpled horn,
-That tossed the dog, 
-That worried the cat, 
-That killed the rat, 
-That ate the malt 
-That lay in the house that Jack built.
-'''
-
-print(poem)
+print('\n'+a[:7]+k[11:]+'\n')
+print(a[:8]+j[9:]+'\n'+k+'\n')
+print(a[:8]+i[12:]+'\n'+j+'\n'+k+'\n')
+print(a[:8]+h[13:]+'\n'+i+'\n'+j+'\n'+k+'\n')
+print(a[:8]+g[12:]+'\n'+h+'\n'+i+'\n'+j+'\n'+k+'\n')
+print(a[:8]+f[12:]+'\n'+g+'\n'+h+'\n'+i+'\n'+j+'\n'+k+'\n')
+print(a[:8]+e[12:]+'\n'+f+'\n'+g+'\n'+h+'\n'+i+'\n'+j+'\n'+k+'\n')
+print(a[:8]+d[13:]+'\n'+e+'\n'+f+'\n'+g+'\n'+h+'\n'+i+'\n'+j+'\n'+k+'\n')
+print(a[:8]+c[11:]+'\n'+d+'\n'+e+'\n'+f+'\n'+g+'\n'+h+'\n'+i+'\n'+j+'\n'+k+'\n')
+print(a[:8]+b[10:]+'\n'+c+'\n'+d+'\n'+e+'\n'+f+'\n'+g+'\n'+h+'\n'+i+'\n'+j+'\n'+k+'\n')
+print(a+'\n'+b+'\n'+c+'\n'+d+'\n'+e+'\n'+f+'\n'+g+'\n'+h+'\n'+i+'\n'+j+'\n'+k)
