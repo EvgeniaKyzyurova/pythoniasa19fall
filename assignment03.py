a823f0932db0f38c0785ae0693155444a89127e6
Kyzyurova Evgenia <kyzyurova.evgenia@gmail.com>
2019-10-29 22:35:00 +0200

    Update assignment03.py

--- assignment03.py
+++ assignment03.py
@@ -1,67 +1,64 @@
 
-def task1():
+def task01(start, end, func):
+    print(','.join(str(i) for i in range(start, end + 1) if func(i) ))
+task01(2000, 3200, lambda i:i % 7 == 0 and i % 5 != 0)
+
+
+def task02(x,y):
+    return [ [i*j for j in range (y)] for i in range (x)]
+print(task02(3,5))
+
+def task03(password):
     """
-    Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5,
-    between 2000 and 3200 (both included). The numbers obtained should be printed in a comma-separated sequence
-    on a single line.
+        >>> task03('ABd1234@1')
+        True
+        >>> task03('a F1#')
+        False
+        >>> task03('2w3E*')
+        False
+        >>> task03('2We3345')
+        False
     """
-    # todo: write your code here
 
-
-def task2(rows, cols):
-    """
-    Write a program which takes 2 digits, X,Y as input and generates a 2-dimensional array.
-    The element value in the i-th row and j-th column of the array should be i*j.
-    Note: i=0,1.., X-1; j=0,1,¡­Y-1.
-
-    Example:
-    Suppose the following inputs are given to the program: 3, 5.
-    Then, the output of the program should be:
-    >>> task2(3, 5)
-    [[0, 0, 0, 0, 0], [0, 1, 2, 3, 4], [0, 2, 4, 6, 8]]
-    """
-    # todo: write your code here
-
-
-def task3(password):
-    """
-    A website requires the users to input username and password to register.
-    Write a program to check the validity of password input by users.
-
-    Following are the criteria for checking the password:
-    1. At least 1 letter between [a-z]
-    2. At least 1 number between [0-9]
-    3. At least 1 letter between [A-Z]
-    4. At least 1 character from [$#@]
-    5. Minimum length of transaction password: 6
-    6. Maximum length of transaction password: 12
-
-    Your program should accept a password and will check them according to the above criteria.
-
-    Example:
-    If the following passwords are given as input to the program: ABd1234@1,a F1#,2w3E*,2We3345
-    Then, the output of the program should be:
-
-    >>> task3('ABd1234@1')
-    True
-    >>> task3('a F1#')
-    False
-    >>> task3('2w3E*')
-    False
-    >>> task3('2We3345')
-    False
-    """
-    # todo: write your code here
-
-
-def task4():
-    """
-    Write password generator function that uses the same rules as in Task 3.
-    The password generator function must be able to generate all possible correct passwords.
-    """
-    # todo: write your code here
-
+    return (6 <= len(password) <= 12
+          and any('a' <= s <= 'z' for s in password)
+          and any('A' <= s <= 'Z' for s in password)
+          and any('0' <= s <= '9' for s in password)
+          and any(s in '$#@' for s in password))
 
 if __name__ == '__main__':
     import doctest
     doctest.testmod()
+
+
+
+import string
+import random
+
+def task05(passw):
+   return (6 <= len(passw) <= 12
+          and any('a' <= s <= 'z' for s in passw)
+          and any('A' <= s <= 'Z' for s in passw)
+          and any('0' <= s <= '9' for s in passw)
+          and any(s in '#$@' for s in passw))
+
+
+def passwo_gen():
+    characters = string.ascii_letters + string.digits + '$#@'
+    passwor =  "".join(random.choice(characters) for x in range(random.randint(8, 16)))
+    if task05(passwor)==True:
+        return passwor
+    else:
+        return False
+
+
+
+if __name__ == '__main__':
+    while True:
+        p = passwo_gen()
+        if p!=False:
+            break
+    print(p)
+
+
+
